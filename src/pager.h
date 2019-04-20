5d2aedfc3282003acaa5092573848d8864aa9759
vic-lee <viclee@nyu.edu>
2019-04-19 23:22:53 -0400

    Initialize pager

---
+++ src/pager.h
@@ -0,0 +1,17 @@
+#ifndef H_PAGER
+#define H_PAGER
+
+#include <iostream>
+
+namespace pager
+{
+
+class Pager
+{
+public:
+    Pager(int machine_size, int page_size, std::string algo_name);
+};
+
+} // namespace pager
+
+#endif
