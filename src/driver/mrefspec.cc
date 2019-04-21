31c901e3d528372497ddee56946dde235862a8eb
vic-lee <viclee@nyu.edu>
2019-04-21 10:44:16 -0400

    Externalize memory reference keyword definition

---
+++ src/driver/mrefspec.cc
@@ -0,0 +1,11 @@
+#include "mrefspec.h"
+
+namespace driver
+{
+
+const RefType SEQ_REF = 10;
+const RefType BACK_REF = 20;
+const RefType JMP_REF = 30;
+const RefType RAND_REF = 40;
+
+} // namespace driver
---
+++ src/driver/mrefspec.h
@@ -0,0 +1,10 @@
+namespace driver
+{
+typedef int RefType;
+
+extern RefType SEQ_REF;  /* Sequential memory reference keyword */
+extern RefType BACK_REF; /* Backward memory reference keyword */
+extern RefType JMP_REF;  /* Jump memory reference keyword */
+extern RefType RAND_REF; /* Random memory reference keyword */
+
+} // namespace driver
