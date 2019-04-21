970cf90533cdba1a850d3408a4321f483c3cbdd3
vic-lee <viclee@nyu.edu>
2019-04-20 21:33:27 -0400

    Create spec for job mixes

---
+++ src/driver/jobmixspec.cc
@@ -0,0 +1,22 @@
+#include "jobmix.h"
+
+namespace driver
+{
+namespace jobmixspecs
+{
+
+const bool SAME_JOBMIX_PER_PROCESS = true;
+const bool DIFF_JOBMIX_PER_PROCESS = false;
+
+JobMix jobmix1 = JobMix(1, SAME_JOBMIX_PER_PROCESS, {JobMixPerProcess{1, 0, 0}});
+JobMix jobmix2 = JobMix(4, SAME_JOBMIX_PER_PROCESS, {JobMixPerProcess{1, 0, 0}});
+JobMix jobmix3 = JobMix(4, SAME_JOBMIX_PER_PROCESS, {JobMixPerProcess{0, 0, 0}});
+JobMix jobmix4 = JobMix(4, DIFF_JOBMIX_PER_PROCESS, {
+                                                        JobMixPerProcess{0.75, 0.25, 0},
+                                                        JobMixPerProcess{0.75, 0, 0.25},
+                                                        JobMixPerProcess{0.75, 0.125, 0.125},
+                                                        JobMixPerProcess{0.5, 0.125, 0.125},
+                                                    });
+
+} // namespace jobmixspecs
+} // namespace driver
