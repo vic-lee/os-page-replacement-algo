d47153edb8472977f0c6a59f0eaaac8ec686a6b7
vic-lee <viclee@nyu.edu>
2019-04-21 00:32:09 -0400

    Implement logic for roundrobin

--- src/driver/driver.cc
+++ src/driver/driver.cc
@@ -1,5 +1,6 @@
 #include "driver.h"
 #include "jobmix.h"
+#include "process.h"
 #include "jobmixspec.h"
 #include "../pager/pager.h"
 
@@ -8,16 +9,43 @@ namespace driver
 Driver::Driver(int proc_size, int job_mix, int ref_count)
     : PROC_SIZE_(proc_size), JOB_MIX_DEF_(job_mix), REF_COUNT_(ref_count), QUANTUM_(3)
 {
+    /* Initialize job mix */
 
     JOB_MIX_ = driver::jobmixspecs::get_jobmix(JOB_MIX_DEF_);
     JOB_MIX_->print();
+
+    /* Initialize runnable processes */
+
+    int num_of_processes = JOB_MIX_->process_count();
+
+    for (int id = 1; id < (num_of_processes + 1); id++)
+        runnable_processes_.push_back(&Process(id, PROC_SIZE_, REF_COUNT_));
 }
 
 void Driver::roundrobin()
 {
+    int quantum_ctr = 0;
+
     while (is_all_process_terminated())
     {
         /* Do process */
+        if (quantum_ctr == QUANTUM_)
+        {
+            Process *front_process = runnable_processes_.front();
+            runnable_processes_.push_back(front_process);
+            runnable_processes_.pop_front();
+            quantum_ctr = 0;
+        }
+
+        Process *current_process = runnable_processes_.front();
+        current_process->do_next_sequential_reference();
+        quantum_ctr++;
+
+        if (current_process->should_terminate())
+        {
+            runnable_processes_.pop_front();
+            quantum_ctr = 0;
+        }
     }
 }
 
