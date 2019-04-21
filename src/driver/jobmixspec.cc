49593bc25db428d69150407c89c1a87bf62d246f
vic-lee <viclee@nyu.edu>
2019-04-20 22:06:41 -0400

    Implement jobmix getter

--- src/driver/jobmixspec.cc
+++ src/driver/jobmixspec.cc
@@ -1,4 +1,5 @@
 #include "jobmix.h"
+#include "jobmixspec.h"
 
 namespace driver
 {
@@ -8,15 +9,37 @@ namespace jobmixspecs
 const bool SAME_JOBMIX_PER_PROCESS = true;
 const bool DIFF_JOBMIX_PER_PROCESS = false;
 
-JobMix jobmix1 = JobMix(1, 1, SAME_JOBMIX_PER_PROCESS, {JobMixPerProcess{1, 0, 0}});
-JobMix jobmix2 = JobMix(2, 4, SAME_JOBMIX_PER_PROCESS, {JobMixPerProcess{1, 0, 0}});
-JobMix jobmix3 = JobMix(3, 4, SAME_JOBMIX_PER_PROCESS, {JobMixPerProcess{0, 0, 0}});
-JobMix jobmix4 = JobMix(4, 4, DIFF_JOBMIX_PER_PROCESS, {
-                                                           JobMixPerProcess{0.75, 0.25, 0},
-                                                           JobMixPerProcess{0.75, 0, 0.25},
-                                                           JobMixPerProcess{0.75, 0.125, 0.125},
-                                                           JobMixPerProcess{0.5, 0.125, 0.125},
-                                                       });
+JobMix jobmix_1 = JobMix(1, 1, SAME_JOBMIX_PER_PROCESS, {JobMixPerProcess{1, 0, 0}});
+JobMix jobmix_2 = JobMix(2, 4, SAME_JOBMIX_PER_PROCESS, {JobMixPerProcess{1, 0, 0}});
+JobMix jobmix_3 = JobMix(3, 4, SAME_JOBMIX_PER_PROCESS, {JobMixPerProcess{0, 0, 0}});
+JobMix jobmix_4 = JobMix(4, 4, DIFF_JOBMIX_PER_PROCESS, {
+                                                            JobMixPerProcess{0.75, 0.25, 0},
+                                                            JobMixPerProcess{0.75, 0, 0.25},
+                                                            JobMixPerProcess{0.75, 0.125, 0.125},
+                                                            JobMixPerProcess{0.5, 0.125, 0.125},
+                                                        });
+
+JobMix *get_jobmix(int id)
+{
+    switch (id)
+    {
+    case 1:
+        return &jobmix_1;
+        break;
+    case 2:
+        return &jobmix_2;
+        break;
+    case 3:
+        return &jobmix_3;
+        break;
+    case 4:
+        return &jobmix_4;
+        break;
+    default:
+        return nullptr;
+        break;
+    }
+}
 
 } // namespace jobmixspecs
 } // namespace driver
