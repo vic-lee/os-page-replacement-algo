0196d721e9e23a58316201291987694e8f503d2b
vic-lee <viclee@nyu.edu>
2019-04-20 21:33:55 -0400

    Initialize jobmix

---
+++ src/driver/jobmix.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "jobmix.h"
+
+namespace driver
+{
+JobMix::JobMix(int proc_count, bool is_uniform, std::vector<JobMixPerProcess> jobmixes)
+    : PROC_COUNT_(proc_count), IS_UNIFORM_(is_uniform), JOBMIXES_(jobmixes)
+{
+    auto calc_rand_ref_dist = [](double a, double b, double c, int s) { return (1 - a - b - c) / (double)s; };
+
+    for (auto &jobmix : JOBMIXES_)
+    {
+        jobmix.rand_ref_dist = calc_rand_ref_dist(jobmix.sequential_ref_dist,
+                                                  jobmix.backward_ref_dist,
+                                                  jobmix.jump_ref_dist,
+                                                  PROC_COUNT_);
+
+        std::cout << "Job mix sequential ref dist (A): " << jobmix.sequential_ref_dist << "\n"
+                  << "Job mix backward ref dist (B): " << jobmix.backward_ref_dist << "\n"
+                  << "Job mix jump ref dist (C): " << jobmix.jump_ref_dist << "\n"
+                  << "Job mix rand ref dist (D): " << jobmix.rand_ref_dist << "\n" << std::endl;
+    }
+}
+} // namespace driver
---
+++ src/driver/jobmix.h
@@ -0,0 +1,25 @@
+#include <vector>
+
+namespace driver
+{
+
+struct JobMixPerProcess
+{
+    double sequential_ref_dist;
+    double backward_ref_dist;
+    double jump_ref_dist;
+    double rand_ref_dist;
+};
+
+class JobMix
+{
+public:
+    JobMix(int proc_count, bool is_uniform, std::vector<JobMixPerProcess> jobmixes);
+    JobMix();
+
+private:
+    const int PROC_COUNT_;
+    const bool IS_UNIFORM_;
+    std::vector<JobMixPerProcess> JOBMIXES_;
+};
+} // namespace driver
