2b13ba18ed8844bba9d72b0fd95c9f23a20e865a
vic-lee <viclee@nyu.edu>
2019-04-21 00:19:21 -0400

    Impelment remmaining references

--- src/driver/process.cc
+++ src/driver/process.cc
@@ -2,8 +2,9 @@
 
 namespace driver
 {
-Process::Process(int id, int proc_size)
-    : ID_(id), SIZE_(proc_size), INIT_CONST_(111), current_ref_addr_(-1) {}
+Process::Process(int id, int proc_size, int ref_count)
+    : ID_(id), SIZE_(proc_size), TOTAL_REF_COUNT_(ref_count),
+      INIT_CONST_(111), current_ref_addr_(-1), remaining_ref_count_(ref_count) {}
 
 void Process::do_next_sequential_reference()
 {
@@ -27,7 +28,15 @@ void Process::do_next_reference(int delta)
 {
     auto init_ref = [this]() -> int { return INIT_CONST_ * ID_ % SIZE_; };
     auto calc_new_ref = [this](int delta) -> int { return (current_ref_addr_ + delta) % SIZE_; };
-    
+
     current_ref_addr_ = (current_ref_addr_ == -1) ? init_ref() : calc_new_ref(delta);
+
+    remaining_ref_count_ = (remaining_ref_count_ == 0) ? 0 : remaining_ref_count_--;
 }
+
+bool Process::should_terminate()
+{
+    return remaining_ref_count_ == 0;
+}
+
 } // namespace driver
--- src/driver/process.h
+++ src/driver/process.h
@@ -3,17 +3,21 @@ namespace driver
 class Process
 {
 public:
-    Process(int id, int proc_size);
+    Process(int id, int proc_size, int ref_count);
     void do_next_sequential_reference();
     void do_next_backward_reference();
     void do_next_jump_reference();
+    bool should_terminate();
 
 private:
     void do_next_reference(int delta);
 
     const int ID_;
     const int SIZE_;
+    const int TOTAL_REF_COUNT_;
     const int INIT_CONST_;
+
     int current_ref_addr_;
+    int remaining_ref_count_;
 };
 } // namespace driverß
