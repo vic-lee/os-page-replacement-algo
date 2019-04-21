18e2d10b77d0aa295bc899aeb1eb6583932ff9d7
vic-lee <viclee@nyu.edu>
2019-04-21 10:45:05 -0400

    Implement reference type calculation logic

--- src/driver/jobmix.cc
+++ src/driver/jobmix.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "jobmix.h"
+#include "mrefspec.h"
 
 namespace driver
 {
@@ -9,18 +10,42 @@ JobMix::JobMix(int id, int proc_count, bool is_uniform, std::vector<JobMixPerPro
 {
     auto calc_rand_ref_dist = [](double a, double b, double c, int s) -> double { return (1 - a - b - c) / (double)s; };
 
+    auto calc_seq_ref_threshold = [](double a) -> double { return a; };
+    auto calc_back_ref_threshold = [](double seqthres, double b) -> double { return seqthres + b; };
+    auto calc_jmp_ref_threshold = [](double backthres, double c) -> double { return backthres + c; };
+
     for (auto &jobmix : JOBMIXES_)
     {
         jobmix.rand_ref_dist = calc_rand_ref_dist(jobmix.sequential_ref_dist,
                                                   jobmix.backward_ref_dist,
                                                   jobmix.jump_ref_dist,
                                                   PROC_COUNT_);
+
+        jobmix.sequential_ref_threshold = calc_seq_ref_threshold(jobmix.sequential_ref_dist);
+        jobmix.backward_ref_threshold = calc_back_ref_threshold(jobmix.sequential_ref_threshold,
+                                                                jobmix.backward_ref_dist);
+        jobmix.jump_ref_threshold = calc_jmp_ref_threshold(jobmix.backward_ref_threshold,
+                                                           jobmix.sequential_ref_dist);
     }
 }
 
 int JobMix::process_count() const
 {
     return PROC_COUNT_;
+}
+
+RefType JobMix::next_ref_type(double quotient, int pid) const
+{
+    int access_idx = IS_UNIFORM_ ? 0 : pid;
+
+    if (0.0 <= quotient <= JOBMIXES_[access_idx].sequential_ref_threshold)
+        return SEQ_REF;
+    else if (quotient <= JOBMIXES_[access_idx].backward_ref_threshold)
+        return BACK_REF;
+    else if (quotient <= JOBMIXES_[access_idx].jump_ref_threshold)
+        return JMP_REF;
+    else
+        return RAND_REF;
 }
 
 void JobMix::print() const
--- src/driver/jobmix.h
+++ src/driver/jobmix.h
@@ -2,13 +2,18 @@
 
 namespace driver
 {
+typedef int RefType;
 
 struct JobMixPerProcess
 {
-    double sequential_ref_dist;
-    double backward_ref_dist;
-    double jump_ref_dist;
-    double rand_ref_dist;
+    double sequential_ref_dist; /* A */
+    double backward_ref_dist;   /* B */
+    double jump_ref_dist;       /* C */
+    double rand_ref_dist;       /* 1 - A - B - C */
+
+    double sequential_ref_threshold; /* A */
+    double backward_ref_threshold;   /* A + B */
+    double jump_ref_threshold;       /* A + B + C */
 };
 
 class JobMix
@@ -16,6 +21,7 @@ class JobMix
 public:
     JobMix(int id, int proc_count, bool is_uniform, std::vector<JobMixPerProcess> jobmixes);
     JobMix();
+    RefType next_ref_type(double quotient, int pid) const;
     int process_count() const;
     void print() const;
 
@@ -23,6 +29,7 @@ private:
     const int ID_;
     const int PROC_COUNT_;
     const bool IS_UNIFORM_;
+
     std::vector<JobMixPerProcess> JOBMIXES_;
 };
 } // namespace driver
