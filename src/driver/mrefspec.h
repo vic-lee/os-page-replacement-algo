9ac55c84faeeda0473070f2964f3fb1c2de8d51b
vic-lee <viclee@nyu.edu>
2019-04-21 11:04:57 -0400

    Add include guard

--- src/driver/mrefspec.h
+++ src/driver/mrefspec.h
@@ -1,3 +1,6 @@
+#ifndef H_MREFSPEC
+#define H_MREFSPEC
+
 namespace driver
 {
 typedef int RefType;
@@ -8,3 +11,5 @@ extern RefType JMP_REF;  /* Jump memory reference keyword */
 extern RefType RAND_REF; /* Random memory reference keyword */
 
 } // namespace driver
+
+#endif
