948ff4c8a6651bb0558c14d9776aff4938016789
vic-lee <viclee@nyu.edu>
2019-04-21 21:11:10 -0400

    Create frames

--- src/pager/pager.cc
+++ src/pager/pager.cc
@@ -3,5 +3,31 @@
 namespace pager
 {
 Pager::Pager(int machine_size, int page_size, std::string algo_name)
-    : MACHINE_SIZE_(machine_size), PAGE_SIZE_(page_size), ALGO_NAME_(algo_name) { }
+    : MACHINE_SIZE_(machine_size), PAGE_SIZE_(page_size),
+      FRAME_COUNT_(MACHINE_SIZE_ / PAGE_SIZE_), ALGO_NAME_(algo_name) 
+{
+    frame_table_ = new Frame[FRAME_COUNT_];
+    next_insertion_idx_ = FRAME_COUNT_ - 1;
+}
+
+Pager::~Pager()
+{
+    delete frame_table_;
+}
+
+bool Pager::push_new_frame(Frame frame)
+{
+    if (next_insertion_idx_-- < 0)
+    {
+        return false;
+    }
+    else
+    {
+        frame_table_[next_insertion_idx_] = frame;
+        next_insertion_idx_--;
+        return true;
+    }
+    
+}
+
 } // namespace pager
--- src/pager/pager.h
+++ src/pager/pager.h
@@ -2,19 +2,31 @@
 #define H_PAGER
 
 #include <iostream>
+#include <vector>
 
 namespace pager
 {
+struct Frame 
+{
+    int pageid;
+    int pid;
+};
 
 class Pager
 {
 public:
     Pager(int machine_size, int page_size, std::string algo_name);
+    ~Pager();
+    bool push_new_frame(Frame frame);
 
 private:
     const int MACHINE_SIZE_;
     const int PAGE_SIZE_;
+    const int FRAME_COUNT_;
     const std::string ALGO_NAME_;
+
+    Frame *frame_table_;
+    int next_insertion_idx_;
 };
 
 } // namespace pager
