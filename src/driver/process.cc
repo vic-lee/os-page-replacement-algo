00cd80c3ebcc0416172b88f73afcb6062690f8a7
vic-lee <viclee@nyu.edu>
2019-04-21 21:26:52 -0400

    Refactor reference methods

--- src/driver/process.cc
+++ src/driver/process.cc
@@ -1,55 +1,36 @@
 #include "process.h"
 #include "mrefspec.h"
+#include "../pager/pager.h"
 
 namespace driver
 {
 const int Process::INIT_CONST_ = 111;
 const int Process::RANDREF_UNDEF_ = 0;
 const int Process::DELTA_UNDEF_ = 0;
+const int Process::DELTA_SEQ_ = 1;
+const int Prcoess::DELTA_BACK_ = -5;
+const int Process::DELTA_JMP_ = 4;
 
 Process::Process(int id, int proc_size, int ref_count)
     : ID_(id), SIZE_(proc_size), TOTAL_REF_COUNT_(ref_count),
       current_ref_addr_(-1), remaining_ref_count_(ref_count) {}
 
-void Process::do_reference_of_type(RefType ref_type, int randref_num)
+void Process::do_reference_of_type(RefType ref_type, int randref_num, pager::Pager pager)
 {
     if (ref_type == INIT_REF)
-        do_initial_reference();
+        do_next_reference(DELTA_UNDEF_, RANDREF_UNDEF_);
 
     else if (ref_type == SEQ_REF)
-        do_next_sequential_reference();
+        do_next_reference(DELTA_SEQ_, RANDREF_UNDEF_);
 
     else if (ref_type == BACK_REF)
-        do_next_backward_reference();
+        do_next_reference(DELTA_BACK_, RANDREF_UNDEF_);
 
     else if (ref_type == JMP_REF)
-        do_next_jump_reference();
+        do_next_reference(DELTA_JMP_, RANDREF_UNDEF_);
 
     else if (ref_type == RAND_REF)
-        do_next_random_reference(randref_num);
-}
-
-void Process::do_next_sequential_reference()
-{
-    int delta = 1;
-    do_next_reference(delta, RANDREF_UNDEF_);
-}
-
-void Process::do_next_backward_reference()
-{
-    int delta = -5;
-    do_next_reference(delta, RANDREF_UNDEF_);
-}
-
-void Process::do_next_jump_reference()
-{
-    int delta = 4;
-    do_next_reference(delta, RANDREF_UNDEF_);
-}
-
-void Process::do_next_random_reference(int randref_num)
-{
-    do_next_reference(DELTA_UNDEF_, randref_num);
+        do_next_reference(DELTA_UNDEF_, randref_num);
 }
 
 void Process::do_next_reference(int delta, int randref_num)
--- src/driver/process.h
+++ src/driver/process.h
@@ -1,6 +1,11 @@
 #ifndef H_PROCESS
 #define H_PROCESS
 
+namespace pager
+{
+class Pager;
+}
+
 namespace driver
 {
 typedef int RefType;
@@ -9,25 +14,24 @@ class Process
 {
 public:
     Process(int id, int proc_size, int ref_count);
-    void do_reference_of_type(RefType ref_type, int randref_num);
+    void do_reference_of_type(RefType ref_type, int randref_num, pager::Pager pager);
 
     bool should_terminate() const;
     int id() const;
 
 private:
-    void do_next_sequential_reference();
-    void do_next_backward_reference();
-    void do_next_jump_reference();
-    void do_next_random_reference(int randref_num);
-    void do_initial_reference();
     void do_next_reference(int delta, int randref_num);
 
     const int ID_;
     const int SIZE_;
     const int TOTAL_REF_COUNT_;
     static const int INIT_CONST_;
-    static const int RANDREF_UNDEF_;
+
+    static const int DELTA_SEQ_;
+    static const int DELTA_BACK_;
+    static const int DELTA_JMP_;
     static const int DELTA_UNDEF_;
+    static const int RANDREF_UNDEF_;
 
     int current_ref_addr_;
     int remaining_ref_count_;
