490957a50b0cef8342b5c0b29d5c5a9e3dbf4c64
vic-lee <viclee@nyu.edu>
2019-04-24 15:42:49 -0400

    Remove unnecessary this reference

--- src/io/randintreader.cc
+++ src/io/randintreader.cc
@@ -25,7 +25,7 @@ RandIntReader::~RandIntReader()
 
 double RandIntReader::calc_next_probability()
 {
-    auto calc_prob = [this](int r) -> double { return r / (MAX_INT_ + 1.0); };
+    auto calc_prob = [](int r) -> double { return r / (MAX_INT_ + 1.0); };
     int randnum = read_next_int();
     return calc_prob(randnum);
 }
