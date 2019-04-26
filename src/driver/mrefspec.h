ef45b6ca785d5062e0f3dedbb6ce1ed941389e2a
vic-lee <viclee@nyu.edu>
2019-04-25 20:48:12 -0400

    Make RefType an Enum

--- src/driver/mrefspec.h
+++ src/driver/mrefspec.h
@@ -3,13 +3,14 @@
 
 namespace driver
 {
-typedef int RefType;
-
-extern const RefType INIT_REF; /* Initial memory reference keyword */
-extern const RefType SEQ_REF;  /* Sequential memory reference keyword */
-extern const RefType BACK_REF; /* Backward memory reference keyword */
-extern const RefType JMP_REF;  /* Jump memory reference keyword */
-extern const RefType RAND_REF; /* Random memory reference keyword */
+enum RefType
+{
+    INIT_REF, /* Initial memory reference keyword */
+    SEQ_REF,  /* Sequential memory reference keyword */
+    BACK_REF, /* Backward memory reference keyword */
+    JMP_REF,  /* Jump memory reference keyword */
+    RAND_REF  /* Random memory reference keyword */
+};
 
 } // namespace driver
 
