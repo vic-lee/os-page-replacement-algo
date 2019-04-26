8f0c53ad85cea269f7d22f803c25ad60854a3e2d
vic-lee <viclee@nyu.edu>
2019-04-25 20:24:51 -0400

    Implement data member getters and setters

--- src/pager/frame.cc
+++ src/pager/frame.cc
@@ -9,6 +9,14 @@ Frame::Frame() : PAGE_ID_(-10), PID_(-10), latest_access_time_(-10){};
 Frame::Frame(int pageid, int pid, int access_time)
     : PAGE_ID_(), PID_(), latest_access_time_(access_time){};
 
+int Frame::pid() const { return PID_; }
+
+int Frame::page_id() const { return PAGE_ID_; }
+
+int Frame::latest_access_time() const { return latest_access_time_; }
+
+void Frame::set_latest_access_time(int t) { latest_access_time_ = t; }
+
 std::ostream &operator<<(std::ostream &stream, const Frame &fr)
 {
     stream << "Frame: \tprocess id " << fr.PID_ << " page id " << fr.PAGE_ID_;
--- src/pager/frame.h
+++ src/pager/frame.h
@@ -11,6 +11,11 @@ public:
     Frame();
     Frame(int pageid, int pid, int access_time);
 
+    int pid() const;
+    int page_id() const;
+    int latest_access_time() const;
+    void set_latest_access_time(int t);
+
     friend std::ostream &operator<<(std::ostream &stream, const Frame &fr);
 
 private:
