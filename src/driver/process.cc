bd79a93c7f61beb525d792a0d2aee45131b287e3
vic-lee <viclee@nyu.edu>
2019-04-29 22:22:04 -0400

    Remove legacy code

--- src/driver/process.cc
+++ src/driver/process.cc
@@ -12,19 +12,14 @@
 
 namespace driver
 {
-const int Process::INIT_CONST_ = 111;
 const int Process::RANDREF_UNDEF_ = 0;
 const int Process::DELTA_UNDEF_ = 0;
 const int Process::REF_ADDR_UNDEF_ = -1;
-const int Process::DELTA_SEQ_ = 1;
-const int Process::DELTA_BACK_ = -5;
-const int Process::DELTA_JMP_ = 4;
 
 Process::Process(int id, int proc_size, int ref_count)
     : ID_(id), SIZE_(proc_size), TOTAL_REF_COUNT_(ref_count),
       current_ref_addr_(REF_ADDR_UNDEF_),
       next_randref_num_(RANDREF_UNDEF_),
-      next_ref_type_(INIT_REF),
       remaining_ref_count_(ref_count)
 {
     nextref_ = std::unique_ptr<memref::Reference>{new memref::InitialReference(ID_)};
@@ -35,56 +30,7 @@ void Process::do_reference(pager::Pager &pager, int access_time)
     if (remaining_ref_count_ == 0)
         return;
 
-    nextref_->simulate(current_ref_addr_, SIZE_, pager, access_time);
-
-    remaining_ref_count_--;
-
-    // switch (next_ref_type_)
-    // {
-    // case INIT_REF:
-    //     do_reference(DELTA_UNDEF_, pager, access_time);
-    //     break;
-
-    // case SEQ_REF:
-    //     do_reference(DELTA_SEQ_, pager, access_time);
-    //     break;
-
-    // case BACK_REF:
-    //     do_reference(DELTA_BACK_, pager, access_time);
-    //     break;
-
-    // case JMP_REF:
-    //     do_reference(DELTA_JMP_, pager, access_time);
-    //     break;
-
-    // case RAND_REF:
-    //     do_reference(DELTA_UNDEF_, pager, access_time);
-    //     break;
-
-    // default:
-    //     break;
-    // }
-}
-
-void Process::do_reference(int delta, pager::Pager &pager, int access_time)
-{
-    if (remaining_ref_count_ == 0)
-        return;
-
-    auto init_ref = [this]() -> int { return (INIT_CONST_ * ID_) % SIZE_; };
-    auto calc_rand_ref = [this](int rand) -> int { return (rand + SIZE_) % SIZE_; };
-    auto calc_new_ref = [this](int delta) -> int { return (current_ref_addr_ + delta + SIZE_) % SIZE_; };
-
-    if (current_ref_addr_ == REF_ADDR_UNDEF_)
-        current_ref_addr_ = init_ref();
-
-    else if (delta != DELTA_UNDEF_)
-        current_ref_addr_ = calc_new_ref(delta);
-
-    else if (next_randref_num_ > RANDREF_UNDEF_)
-        current_ref_addr_ = calc_rand_ref(next_randref_num_);
-
-    pager.reference_by_virtual_addr(current_ref_addr_, ID_, access_time);
+    current_ref_addr_ = nextref_->simulate(current_ref_addr_, SIZE_, pager, access_time);
 
     remaining_ref_count_--;
 }
--- src/driver/process.h
+++ src/driver/process.h
@@ -34,10 +34,6 @@ public:
     bool should_terminate() const;
     int id() const;
 
-    // Process &operator=(const Process &pr);
-    // Process &operator=(Process &&pr);
-    // Process(Process &&pr);
-
     friend std::ostream &operator<<(std::ostream &stream, const Process &p);
 
 private:
@@ -48,21 +44,13 @@ private:
     const int SIZE_;
     const int TOTAL_REF_COUNT_;
 
-    static const int INIT_CONST_; /* Constant factor for initializing reference address */
-
-    static const int DELTA_SEQ_;  /* Change in memory reference when in sequential reference mode */
-    static const int DELTA_BACK_; /* Change in memory reference when in backward reference mode */
-    static const int DELTA_JMP_;  /* Change in memory reference when in jump reference mode */
-
     static const int DELTA_UNDEF_;    /* Special status code signifying Delta is undefined */
     static const int RANDREF_UNDEF_;  /* Special status code signifying random reference number is undefined */
     static const int REF_ADDR_UNDEF_; /* Current reference address undefined (uninitialized) */
 
     int current_ref_addr_;
     int next_randref_num_;
-    RefType next_ref_type_;
     std::shared_ptr<memref::Reference> nextref_;
-    // memref::Reference *p_nextref_;
     int remaining_ref_count_;
 };
 } // namespace driver
