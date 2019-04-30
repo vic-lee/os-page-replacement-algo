5854fa84dcda83ceee530b27793872c5b152c05b
vic-lee <viclee@nyu.edu>
2019-04-29 22:37:37 -0400

    Constructing RandomReference requires random number

--- src/memref/memref_rand.cc
+++ src/memref/memref_rand.cc
@@ -5,6 +5,15 @@
 namespace memref
 {
 
-RandomReference::RandomReference(int pid) : Reference(pid, 0) {}
+RandomReference::RandomReference(int pid, int randnum) : Reference(pid, 0), randnum_(randnum) {}
+
+int RandomReference::simulate(int ref_input, int proc_size, pager::Pager &pager, int time_accessed)
+{
+    int viraddr = (randnum_ + proc_size) % proc_size;
+
+    pager.reference_by_virtual_addr(viraddr, pid_, time_accessed);
+
+    return viraddr;
+}
 
 } // namespace memref
--- src/memref/memref_rand.h
+++ src/memref/memref_rand.h
@@ -8,7 +8,11 @@ namespace memref
 class RandomReference : public Reference
 {
 public:
-    RandomReference(int pid);
+    RandomReference(int pid, int randnum);
+    int simulate(int ref_input, int proc_size, pager::Pager &pager, int time_accessed) override;
+
+private:
+    int randnum_;
 };
 
 } // namespace memref
